49be91bc2322dc4eee62a707202146bb7215c53e
iredstonne <iredstonne.dev@gmail.com>
2026-07-16 23:20:11 +0200

    feat: Make navbar brand fully configurable

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+export type SiteConfig = {
+    name: string,
+    homeUrl: string
+}
+
+export const site = {
+    name: "Portfolio de Kristofer Gehringer",
+    homeUrl: "/portfolio"
+} as SiteConfig
