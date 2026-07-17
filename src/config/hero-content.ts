dcba1d50b09c07688622ad8bd50052de4424fff9
iredstonne <iredstonne.dev@gmail.com>
2026-07-17 12:12:01 +0200

    feat: Replace MDX with config file to configure hero

---
+++ src/config/hero-content.ts
@@ -0,0 +1,9 @@
+export type HeroContentConfig = {
+  title: string
+  description: string
+}
+
+export const heroContent = {
+    title: "Des solutions numériques pour de vrais besoins",
+    description: "Développeur autodidacte basé à Bruxelles, je conçois des solutions fonctionnelles, fiables et évolutives.",
+} as HeroContentConfig
